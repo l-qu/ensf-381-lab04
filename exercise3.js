c4ff43de2b0e61e27ec28f6a30ad7e3bd37a7f02
gianna-k0520 <gianna.kong@ucalgary.ca>
2026-02-27 13:31:56 -0700

    Finished questions 7, 10, and 11

--- exercise3.js
+++ exercise3.js
@@ -1,6 +1,6 @@
-async function retrieveData(){
+async function retrieveData(url){
     try{
-        var userData = await fetch("https://69a1da772e82ee536fa26007.mockapi.io/users_api");
+        var userData = await fetch(url);
         var jsonData = await userData.json();
 
         render(jsonData);
@@ -24,8 +24,7 @@ function render(users){
                 <p>first_name: ${users[i].first_name ?? ""}</p>
                 <p>user_group: ${users[i].user_group ?? ""}</p>
                 <p>id: ${users[i].id ?? ""}</p>
-            </article>
-            `;
+            </article>`;
             firstRound = false;
          }else{
             userGridID.innerHTML += `
@@ -34,24 +33,45 @@ function render(users){
                 <p>first_name: ${users[i].first_name ?? ""}</p>
                 <p>user_group: ${users[i].user_group ?? ""}</p>
                 <p>id: ${users[i].id ?? ""}</p>
-            </article>
-            `;
+            </article>`;
          }
-        }
-
-        
-
+        }    
 }
-var userGrid = document.getElementsByClassName("container")
+var userGrid = document.getElementsByClassName("container");
+var userGridID = document.getElementById("userGrid")
 var viewToggleBtn = document.getElementById('viewToggleBtn');
 var deleteIdInput = document.getElementById('deleteIdInput');
 var deleteBtn = document.getElementById('deleteBtn');
 var sortByGroupBtn = document.getElementById('sortByGroupBrn');
 var sortbyIdBtn = document.getElementById('sortByIdBtn');
 
+var users = retrieveData("https://69a1da772e82ee536fa26007.mockapi.io/users_api").then(console.log);
 
-var users = retrieveData().then(console.log);
+viewToggleBtn.addEventListener('click', (event) => {
+    if (userGridID.classList.contains("grid-view")){
+        userGridID.classList.replace("grid-view", "list-view");
+    } else if(userGridID.classList.contains("list-view")){
+        userGridID.classList.replace("list-view", "grid-view");
+    }
+})
 
-console.log(users);
+deleteBtn.addEventListener('click', (event) =>{
+    var idDelete = deleteIdInput.value;
+    try{
+        fetch(`https://69a1da772e82ee536fa26007.mockapi.io/users_api/${idDelete}`, {
+            method: 'DELETE',
+        }).then(response => {
+            if (!response.ok){
+                console.error("No user by that id was found.")
+            }
+            users = retrieveData("https://69a1da772e82ee536fa26007.mockapi.io/users_api").then(console.log);
+        })
+    }catch(error){
+        console.log("No user by that id was found.");
+    }
+
+    
+})
+
 
 
