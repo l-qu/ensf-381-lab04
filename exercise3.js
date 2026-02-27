a069b985df9f7a1edeeb24371ef2e02bdab9ac45
gianna-k0520 <gianna.kong@ucalgary.ca>
2026-02-27 12:33:17 -0700

    Did some of exercise 3

---
+++ exercise3.js
@@ -0,0 +1,56 @@
+async function retrieveData(){
+    try{
+        var userData = await fetch("https://69a1da772e82ee536fa26007.mockapi.io/users_api");
+        var jsonData = await userData.json();
+
+        render(jsonData);
+
+        return jsonData
+
+    } catch (error){
+        console.error("Error fetching data", error);
+    }
+}
+
+function render(users){
+    var userGridID = document.getElementById('userGrid');
+    var firstRound = true;
+
+    for (let i = 0; i < users.length; i++){
+        if(firstRound == true){
+            userGridID.innerHTML = `
+            <article class="user-card">
+                <h3>${users[i].first_name ?? ""}</h3>
+                <p>first_name: ${users[i].first_name ?? ""}</p>
+                <p>user_group: ${users[i].user_group ?? ""}</p>
+                <p>id: ${users[i].id ?? ""}</p>
+            </article>
+            `;
+            firstRound = false;
+         }else{
+            userGridID.innerHTML += `
+            <article class="user-card">
+                <h3>${users[i].first_name ?? ""}</h3>
+                <p>first_name: ${users[i].first_name ?? ""}</p>
+                <p>user_group: ${users[i].user_group ?? ""}</p>
+                <p>id: ${users[i].id ?? ""}</p>
+            </article>
+            `;
+         }
+        }
+
+        
+
+}
+var toggelBtnID = document.getElementById('viewToggleBtn');
+var deleteIdInputID = document.getElementById('deleteIdInput');
+var deletebtnID = document.getElementById('deleteBtn');
+var sortByGroupID = document.getElementById('sortByGroupBrn');
+var sortbyIdID = document.getElementById('sortByIdBtn');
+
+
+var users = retrieveData().then(console.log);
+
+console.log(users);
+
+
